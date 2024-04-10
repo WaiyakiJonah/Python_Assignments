a9f8d6b33098292bda42b8e9c8e90fc97e547c51
WaiyakiJonah <124672798+WaiyakiJonah@users.noreply.github.com>
2024-04-10 18:16:10 +0300

    Add files via upload

---
+++ Python OOP Assignment.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+
+person1 = Person("Jonathan", 30, "Male")
+print(person1.name)   
+print(person1.age)     
+print(person1.gender)  
